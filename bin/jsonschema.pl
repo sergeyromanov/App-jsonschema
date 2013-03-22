4debf6f912f43b60dc92bfa265c918ea8e9402e1
André Santos <andrefs@cpan.org>
2013-03-22 17:48:49 +0000

    POD and cleaning for dzilla

--- bin/jsonschema.pl
+++ bin/jsonschema.pl
@@ -3,7 +3,6 @@
 use strict; use warnings;
 use App::jsonschema;
 
-#ABSTRACT: Validate JSON files using JSON Schema
 		
 &usage if @ARGV < 2;
 my $schema_file = shift;
@@ -16,3 +15,37 @@ sub usage {
 	exit 1;
 }
 
+
+# ABSTRACT: Validate JSON files using JSON Schema
+# PODNAME: jsonschema.pl
+
+=head1 SYNOPSIS
+
+jsonschema.pl schema.json file1.json [file2.json ...]
+
+=head1 COMMANDS
+
+=over 4
+
+
+=back
+
+=head1 OPTIONS
+
+=over 4
+
+=back
+
+=head1 SEE ALSO
+
+L<App::jsonschema>
+
+=head1 COPYRIGHT
+
+Copyright 2013 Andre Santos.
+
+=head1 AUTHOR
+
+Andre Santos <andrefs@cpan.org>
+
+=cut
